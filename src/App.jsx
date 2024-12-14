a1f4b7224d9798ba58dfbcd3ce0b1e93f69ceece
Pratik <pratikkhatri520@gmail.com>
2024-12-14 21:16:59 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import RealTimeEventsPage from "./pages/RealTimeEventsPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <>
-      set up
-    </>
-  )
+    <Router>
+      <Navbar />
+      <RealTimeEventsPage />
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
